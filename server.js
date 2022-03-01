bb2e4badfdc9df5b85274c65b5a90169335e7c29
JayMoses01 <jayrmoses@gmail.com>
2022-02-28 23:23:03 -0800

    Saves notes now

--- server.js
+++ server.js
@@ -29,74 +29,49 @@ app.get("/api/notes", (req, res) => {
 
 // POST request to add a note
 app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a note`);
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    // Obtain existing notes
+
+    
+        // Convert string into JSON object
+
+        // Add a new note
+        db.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(db),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
   
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        id: uuid(),
-      };
-  
-      // Obtain existing notes
-      fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-  
-          // Add a new note
-          parsedNotes.push(newNote);
-  
-          // Write updated notes back to the file
-          fs.writeFile(
-            './db/db.json',
-            JSON.stringify(parsedNotes, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated notes!')
-          );
-        }
-      });
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      console.log(newNote); //JRM: I just added.
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in adding note');
-    }
-  });
+
+    res.sendStatus(200);
+  } else {
+    res.status(500).json('Error in adding note');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Note Taker app is listening at http://localhost:${PORT}`)
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
