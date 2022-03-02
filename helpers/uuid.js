7c71ab21d24298f52344b3d8bd8162fae79f9b66
JayMoses01 <jayrmoses@gmail.com>
2022-03-01 22:41:03 -0800

    Adding descriptive comments to code

--- helpers/uuid.js
+++ helpers/uuid.js
@@ -1,4 +1,4 @@
-// Immediately export a function that generates a string of random numbers and letters
+// JRM: This function exports a function that generates a random string of letters and numbers to use as the note ID.
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
--- server.js
+++ server.js
@@ -1,112 +1,70 @@
+// JRM: These variables link the requires package, paths, and files.
 const express = require("express");
 const path = require('path');
 const fs = require('fs');
 let db = require("./db/db.json");
-
-const app = express();
 const uuid = require('./helpers/uuid.js');
 
 require('dotenv');
 const PORT = process.env.PORT || 3001;
 
+// JRM: Activates Express application.
+const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
+// JRM: GET request for landing page.
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+// JRM: GET request for the Notes page.
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+// JRM: GET request to obtain created notes from where they are saved (in the db.json file).
 app.get("/api/notes", (req, res) => {
   res.status(200).json(db)
 });
 
-// POST request to add a note
+// JRM: POST request to add a new note.
 app.post('/api/notes', (req, res) => {
-  // Log that a POST request was received
+  // JRM: Logs a message that a POST request was received to add a note.
   console.info(`${req.method} request received to add a note`);
 
-  // Destructuring assignment for the items in req.body
+  // JRM: Destructuring req.body for note title and text contents.
   const { title, text } = req.body;
 
-  // If all the required properties are present
+  // JRM: Condition to require both note title and text to create the note.
   if (title && text) {
-    // Variable for the object we will save
+    // Variable for the created note object, including a random note ID.
     const newNote = {
       title,
       text,
       id: uuid(),
     };
+      // JRM: Adds the new note into the note storage file (db.json).
+      db.push(newNote);
 
-        // Add a new note
-        db.push(newNote);
-
-        // Write updated notes back to the file
-        fs.writeFile(
-          './db/db.json',
-          JSON.stringify(db),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated notes!')
-        );
-  
-
+      // JRM: Writes the updated notes to db.json again. Sends an error message if the required title and text are not present and does not save the new note.
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(db),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated notes!')
+      );
     res.sendStatus(200);
   } else {
     res.status(500).json('Error in adding note');
   }
 });
 
-
-//DELETE request to delete a note. NOT FINISHED YET, BUT EVERYTHING ELSE WORKS.
-/*
-app.delete('api/notes/:id', (req,res) => {
-  // Log that a DELETE request was received
-  console.info(`${req.method} request received to delete note ${req.params.id}`);
-
-    // Obtain existing notes
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        // Convert string into JSON object
-        const parsedReviews = JSON.parse(data);
-
-
-        // Delete note
-        param("id", (req, res, next, id) => {
-          console.log(id)
-          next()
-        })
-
-        // Write updated notes back to the file
-        fs.writeFile(
-          './db/db.json',
-          JSON.stringify(db),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated notes!')
-        );
-
-      res.sendStatus(200);
-    } else {
-      res.status(500).json('Error in adding note');
-    }
-  });
-*/
-
-
-
+// JRM: Triggers the application to listen on a port.
 app.listen(PORT, () => {
   console.log(`Note Taker app is listening at http://localhost:${PORT}`)
 });
-
-
-
